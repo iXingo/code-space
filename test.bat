d6749f602e5fc2b9009d773878d9c4b54bf4128f
iXingo <ixingo@126.com>
2017-03-30 09:05:20 +0800

    turns fucking green

--- test.bat
+++ test.bat
@@ -3,7 +3,7 @@
 set /a var+=1
 echo a >> test.txtgit 
 git add .
-git commit --date=" April %var% 9:05:20 2016 +0800" -am "turns fucking green"
+git commit --date=" April %var% 9:05:20 2017 +0800" -am "turns fucking green"
 if %var% leq 30 GOTO start
 git push origin master
 @echo Mission finished !
