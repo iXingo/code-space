5e01fc5d0faeb0220a03afcd5759d3e3c264660c
iXingo <ixingo@126.com>
2017-05-11 22:15:28 +0800

    update

---
+++ src/cn.iwangxin/sort/TestMain.java
@@ -0,0 +1,20 @@
+package cn.iwangxin.sort;
+
+import cn.iwangxin.sort.template.SortTemplate;
+import cn.iwangxin.sort.heapsort.Heap;
+
+import java.util.Scanner;
+
+public class TestMain {
+    public static void main(String[] args) {
+        SortTemplate sortAlgoithm = new Heap();
+        Scanner s = new Scanner(System.in);
+        System.out.println("请输入字符串：");
+        String strr = s.nextLine();
+        System.out.println(">>>" + strr);
+        String[] str = strr.split(" ");
+        sortAlgoithm.sort(str);
+        //assert(SortTemplate.isSorted(str));
+        SortTemplate.show(str);
+    }
+}
---
+++ src/cn.iwangxin/sort/quicksort/Quick.java
@@ -0,0 +1,33 @@
+package cn.iwangxin.sort.quicksort;
+
+import cn.wangxin.sort.template.SortTemplate;
+
+/**
+ * Created by 汪欣 on 2017/02/17.
+ */
+public class Quick extends SortTemplate{
+    @Override
+    public void sort(Comparable[] data) {
+        sort(data,0,data.length -1);
+    }
+
+    private static void sort(Comparable[] data, int lo, int hi) {
+        if(hi <= lo) return;
+        int j = partition(data,lo,hi);
+        sort(data,lo ,j-1);
+        sort(data,j+1,hi);
+    }
+
+    private static int partition(Comparable[] data,int lo ,int hi){
+        int i = lo,j= hi + 1;
+        Comparable v =data[lo];
+        while(true){
+            while(less(data[++i],v)) if(i == hi) break;
+            while(less(v,data[--j])) if(j == lo) break;
+            if(i >= j) break;
+            exch(data,i,j);
+        }
+        exch(data,lo,j);
+        return j;
+    }
+}
