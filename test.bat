1986a4f2c2b719a365a40f6b4c77273e6c2fc0b4
Shawn Wang <ixingo@126.com>
2018-09-09 21:15:52 +0800

    Delete test.bat

--- test.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:start
-set /a var+=1
-echo a >> test.txtgit 
-git add .
-git commit --date=" May %var% 9:05:20 2017 +0800" -am "turns fucking green"
-if %var% leq 30 GOTO start
-git push origin master
-@echo Mission finished !
